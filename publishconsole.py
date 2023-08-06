512f6b22ac57d8a198b26bf1f3556cf62eb7a86a
rshouker <rshouker@gmail.com>
2023-08-06 17:14:42 +0300

    encapsulate fs etc, so can be replaced for testing

---
+++ publishconsole.py
@@ -0,0 +1,4 @@
+from config import collect_slides, SlidesCollection # , ShowConfig
+from kivy import App, Widget, ScreenManager, Screen, Image, Label, BoxLayout
+from kivy import Button, Popup, TextInput, Slider, ToggleButton
+  
